7c14f855b3da4e046ee9966ce5a40e80c3ef03b2
user <caioenrique.na@gmail.com>
2024-11-25 14:59:26 -0300

    criação do ProfessorController/ modelagem do professor e regra de negocio no ProfessorService

--- api/src/main/java/br/com/alunoonline/api/model/Professor.java
+++ api/src/main/java/br/com/alunoonline/api/model/Professor.java
@@ -1,2 +1,31 @@
-package br.com.alunoonline.api.model;public class Professor {
+package br.com.alunoonline.api.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Professor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String cpf;
+
+
 }
