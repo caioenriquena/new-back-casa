73d7cdf9b9300ba88bad84fb17075dfd36f9deda
user <caioenrique.na@gmail.com>
2024-11-25 15:51:08 -0300

    criando disciplina

---
+++ api/src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
@@ -0,0 +1,19 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Disciplina;
+import br.com.alunoonline.api.service.DisciplinaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/disciplinas")
+public class DisciplinaController {
+    @Autowired
+    DisciplinaService disciplinaService;
+
+    public void criarDisciplina (@RequestBody Disciplina disciplina){
+        disciplinaService.criarDisciplina(disciplina);
+    }
+}
