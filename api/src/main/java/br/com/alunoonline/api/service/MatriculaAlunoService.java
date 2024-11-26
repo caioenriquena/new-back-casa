1170774fcf38936673c677df741bb451bfb6aeb9
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2024-11-26 10:36:53 -0300

    regra de negocio de trancar matricula

--- api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
+++ api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -4,14 +4,25 @@ import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.repository.MatriculaAlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MatriculaAlunoService {
     @Autowired
     MatriculaAlunoRepository matriculaAlunoRepository;
+
     public void criarMatricula(MatriculaAluno matriculaAluno){
         matriculaAluno.setStatus(MatriculaAlunoStatusEnum.MATRICULADO);
         matriculaAlunoRepository.save(matriculaAluno);
+    }
+
+    public void trancarMatricula(Long matriculaAlunoId){
+        MatriculaAluno matriculaAluno =
+                matriculaAlunoRepository.findById(matriculaAlunoId)
+                        .orElseThrow(()->
+                                new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                        "Matricula do Aluno não encontrada !"));
     }
 }
